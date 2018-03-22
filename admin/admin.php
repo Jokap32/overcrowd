12205daae05f5e3b6855c757ed9a5a3c502795cd
Michael Lu <mikeylu95@gmail.com>
2018-03-22 16:39:27 -0400

    Add files via upload
    Aesthetic Update

--- admin/admin.php
+++ admin/admin.php
@@ -6,29 +6,34 @@
 body {font-family: Arial, Helvetica, sans-serif;}
 
 /* Full-width input fields */
-input[type=text], input[type=password] {
-    width: 100%;
-    padding: 12px 20px;
-    margin: 8px 0;
-    display: inline-block;
-    border: 1px solid #ccc;
-    box-sizing: border-box;
+
+h2{
+ text-align: center;
+ margin: 0px auto;  
+ margin-top: 2%;
+ margin-bottom: 2%; 
 }
 
+body{
+    text-align: center;
+}
 /* Set a style for all buttons */
 button {
     background-color: #4CAF50;
     color: white;
     padding: 14px 20px;
-    margin: 8px 0;
     border: none;
     cursor: pointer;
     width: 100%;
+    margin: 0px auto;
+    display: block;
+    text-align: center;
 }
 
 button:hover {
     opacity: 0.8;
 }
+
 
 /* Extra styles for the cancel button */
 .cancelbtn {
@@ -38,19 +43,10 @@ button:hover {
 }
 
 /* Center the image and position the close button */
-.imgcontainer {
-    text-align: center;
-    margin: 24px 0 12px 0;
-    position: relative;
-}
 
 img.avatar {
     width: 40%;
     border-radius: 50%;
-}
-
-.container {
-    padding: 16px;
 }
 
 span.psw {
@@ -62,6 +58,8 @@ span.psw {
 .modal {
     display: none; /* Hidden by default */
     position: fixed; /* Stay in place */
+    text-align: center;
+    margin: 0px auto;
     z-index: 1; /* Sit on top */
     left: 0;
     top: 0;
@@ -127,17 +125,11 @@ span.psw {
 </head>
 <body>
     
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAgWc5mUhyrIwyRYQNxPbgqtc1VhwoNjRs&callback=myMap"></script>
-
-<div class = "contentWrapper">
-
-<div class = "contactForm">
-<script>
+<!-- <script>
 function resetForm(){
 	document.getElementById("contact_done").reset();
 }
-</script>
-
+</script> -->
 <?php
 function checkName($uname){
 	
@@ -211,6 +203,19 @@ else {
     }
     </script>
 
+
+    <script>
+        function signOut(){
+            var auth2 = gapi.auth2.getAuthInstance();
+            auth2.signOut().then(function()){
+                alert("You have been successfully signed out!");
+
+                $(".g-signin2").css("display","block");
+                $(".data").css("display","none");
+            }
+        }
+    </script>
+
     <meta name="google-signin-client_id" content="663095738920-fim1c65ta26o7tt39dbr4j048vn4c46h.apps.googleusercontent.com">
 
     <script type="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -218,18 +223,17 @@ else {
     <script src="script.js"></script>
     <style>
       .g-signin2{
-        margin-left: 500px;
-        margin-top: 200px;
+        margin: 0px auto;
+        display: block;
+        text-align: center;
+        max-width: 6%;
       }
     </style>
 
 <div class="g-signin2" data-onsuccess="onSignIn"></div>
 <div class="data">
-  <p>Profile Details</p>
-  <img id="pic" class="img-circle" width="100" height="100"/>
-  <p>Email Address</p>
   <p id="email" class="alert-danger"></p>
-  <button onclick="signOut()" class="btn btn-danger">Sign Out</button>
+  <button onclick="signOut()" class="btn btn-danger" style="width: auto; text-align:center";>Sign Out</button>
 </div>
 
 
@@ -237,9 +241,9 @@ else {
 }?>
 
 </div>
-</div>
 
 <h2>ADMIN LOGIN</h2>
+
 
 <button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Login</button>
 
